e519b227ec82bc141b173b914075553c44794896
CarlJerick.Peñaflor <Carl-Jerick@users.noreply.github.com>
2020-10-23 12:36:08 +0800

    =)

---
+++ ADETQ3_carljerick_penaflor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADETQ3_CarlJerick_Penaflor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("info");
+        }
+
+        public IActionResult Abt()
+        {
+            ViewData["Message"] = "Penaflor, Carl Jerick D.";
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ ADETQ3_carljerick_penaflor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_carljerick_penaflor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
