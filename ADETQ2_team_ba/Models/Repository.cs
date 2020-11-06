d1566c026c47e8c601c6f0a1fad7f59a39ffdb0d
CarlJerick.Peñaflor <Carl-Jerick@users.noreply.github.com>
2020-11-06 12:41:44 +0800

    =>

---
+++ ADETQ2_team_ba/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enrollment.Models
+{
+    public static class Repository
+    {
+        private static List<admission> enrolled = new List<admission>();
+
+        public static IEnumerable<admission> Students => enrolled;
+
+        public static void Addenrolled(admission enroll)
+        {
+            enrolled.Add(enroll);
+        }
+
+    }
+}
---
+++ ADETQ2_team_ba/Models/admission.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Enrollment.Models
+{
+    public class admission
+    {
+        [Required(ErrorMessage = "Student Name:")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Student Number:")]
+        public string No { get; set; }
+        [Required(ErrorMessage = "Age:")]
+        public string Age { get; set; }
+        [Required(ErrorMessage = "Address")]
+        public string address { get; set; }
+    }
+}
